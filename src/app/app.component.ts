214bd94945c6844c836b0ae047b657f9e6616a0c
naushadwebdesigner <naushad.webdesigner@gmail.com>
2025-03-24 15:07:54 +0530

    rouuting complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {  RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./comps/header/header.component";
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,  HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './comps/home/home.component';
+import { AboutComponent } from './comps/about/about.component';
+import { LoginComponent } from './comps/login/login.component';
+import { ContactComponent } from './comps/contact/contact.component';
+import { PageNotFoundComponent } from './comps/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: '**',
+    component:PageNotFoundComponent,
+  }
+];
